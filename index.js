3e5a3fbfe00fdf4ce47d0c435b67333e319528e0
Ezra Oduor <ezrachurchill20@gmail.com>
2025-03-22 18:20:55 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email) {
+     return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers : {
+            "Content-Type" : "application/json",
+            Accept : "application/json"
+        },
+        body: JSON.stringify({name, email})
+    })
+    .then((response) => {
+        if (!response.ok) {
+          return new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        document.body.innerHTML += `<p>ID: ${data.id}</p>`;
+      })
+      .catch((error) => {
+        document.body.innerHTML += `<p>${error.message}</p>`;
+      });
+  }
+
